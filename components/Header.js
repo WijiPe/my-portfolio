f4841efd7eb5e97b1684c2f6455749b4acf1d1be
Wiji <peawseaw@gmail.com>
2022-08-09 09:17:51 -0700

    fixing nav bar on meterial ui

--- components/Header.js
+++ components/Header.js
@@ -11,41 +11,35 @@ import { makeStyles } from '@mui/styles';
 const useStyles = makeStyles((theme) => {
     return{
         appbar:{
-            width: '70%',
             position: "fixed",
-            marginRight: 250
         },
         // toolbar: theme.mixins.toolbar
     }
 })
 
-  export default function Header() {
+export default function Header() {
     const classes = useStyles();
     return (
-        <div className={classes.toolbar}>
-            <Box sx={{ 
-        flexGrow: 1,
-        }}>
-        <AppBar className={classes.appbar} style={{backgroundColor:'#F85F36'}} >
-          <Toolbar>
-            <IconButton
-              size="large"
-              edge="start"
-              color="inherit"
-              aria-label="menu"
-              sx={{ mr: 2 }}
-            >
-              <MenuIcon />
-            </IconButton>
-            <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-              News
-            </Typography>
-            <Button color="inherit">Login</Button>
-          </Toolbar>
-        </AppBar>
-      </Box>
-
-        </div>
-      
+        <Box sx={{ 
+            flexGrow: 1,
+            }}>
+            <AppBar className={classes.appbar} style={{backgroundColor:'#F85F36'}} >
+                <Toolbar>
+                    <IconButton
+                        size="large"
+                        edge="start"
+                        color="inherit"
+                        aria-label="menu"
+                        sx={{ mr: 2 }}
+                    >       
+                        <MenuIcon />
+                    </IconButton>
+                    <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+                        News
+                    </Typography>
+                    <Button color="inherit">Login</Button>
+                </Toolbar>
+            </AppBar>
+        </Box>
     );
-  }
+}
